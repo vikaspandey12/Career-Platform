d0e040df352bb24c2b0cbedfaac57da5401e8372
vikaspandey12 <137185677+vikaspandey12@users.noreply.github.com>
2026-03-27 11:50:42 +0530

    Use environment variable for server port

--- server.ts
+++ server.ts
@@ -5,7 +5,7 @@ import * as gemini from "./src/services/geminiServer";
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = process.env.PORT || 3000;
 
   app.use(express.json());
 
